a987a4e62b9078d8a945ba15cb016e9db95f586c
摸鱼喵 <33485524+LavenderQAQ@users.noreply.github.com>
2022-11-28 19:53:16 +0800

    Complete registering goroutine and starting goroutine to complete basic data flow (#8)
    * define parser interface and write simpleparse
    
    * Complete registering goroutine and starting goroutine to complete basic data flow

---
+++ pkg/parser/core.go
@@ -0,0 +1,53 @@
+package parser
+
+import (
+	"encoding/json"
+
+	"github.com/kasterism/astermule/pkg/dag"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	logger *logrus.Logger
+)
+
+func SetLogger(log *logrus.Logger) {
+	logger = log
+}
+
+type ControlPlane struct {
+	Fs    []func()
+	Entry []chan<- Message
+	Exit  []<-chan Message
+}
+
+type Parser interface {
+	Parse(*dag.DAG) ControlPlane
+}
+
+type Message struct {
+	Status Status      `json:"status"`
+	Data   interface{} `json:"data"`
+}
+
+// TODO: Define Status
+type Status struct {
+	Health bool
+}
+
+func NewMessage(health bool, data interface{}) *Message {
+	return &Message{
+		Status: Status{
+			Health: health,
+		},
+		Data: data,
+	}
+}
+
+func (in *Message) DeepMergeInto(out *Message) {
+	// TODO: Merge Json
+}
+
+func (m Message) Parse() ([]byte, error) {
+	return json.Marshal(m)
+}
---
+++ pkg/parser/simpleparser.go
@@ -0,0 +1,98 @@
+package parser
+
+import (
+	"github.com/kasterism/astermule/pkg/dag"
+)
+
+type SimpleParser struct {
+	ChanGroup map[string]*ChannelGroup
+}
+
+type ChannelGroup struct {
+	ReadCh  []<-chan Message
+	WriteCh []chan<- Message
+}
+
+func NewSimpleParser() *SimpleParser {
+	return &SimpleParser{
+		ChanGroup: make(map[string]*ChannelGroup),
+	}
+}
+
+func (s *SimpleParser) Parse(d *dag.DAG) ControlPlane {
+	c := ControlPlane{}
+	s.Init(d)
+	s.makeChannelGroup(d)
+	c.Entry, c.Exit = s.scanChannelGroup(d)
+	c.Fs = s.makeFunc(d)
+	return c
+}
+
+func (s *SimpleParser) Init(d *dag.DAG) {
+	for _, node := range d.Nodes {
+		s.ChanGroup[node.Name] = &ChannelGroup{
+			ReadCh:  make([]<-chan Message, 0),
+			WriteCh: make([]chan<- Message, 0),
+		}
+	}
+}
+
+func (s *SimpleParser) makeChannelGroup(d *dag.DAG) {
+	for _, node := range d.Nodes {
+		for _, dep := range node.Dependencies {
+			ch := make(chan Message)
+			s.ChanGroup[dep].WriteCh = append(s.ChanGroup[dep].WriteCh, ch)
+			s.ChanGroup[node.Name].ReadCh = append(s.ChanGroup[node.Name].ReadCh, ch)
+		}
+	}
+}
+
+func (s *SimpleParser) scanChannelGroup(d *dag.DAG) ([]chan<- Message, []<-chan Message) {
+	entry := make([]chan<- Message, 0)
+	exit := make([]<-chan Message, 0)
+	for _, v := range s.ChanGroup {
+		if len(v.ReadCh) == 0 {
+			ch := make(chan Message)
+			entry = append(entry, ch)
+			v.ReadCh = append(v.ReadCh, ch)
+		}
+
+		if len(v.WriteCh) == 0 {
+			ch := make(chan Message)
+			exit = append(exit, ch)
+			v.WriteCh = append(v.WriteCh, ch)
+		}
+	}
+	return entry, exit
+}
+
+func (s *SimpleParser) makeFunc(d *dag.DAG) []func() {
+	fs := make([]func(), 0)
+	for _, node := range d.Nodes {
+		chGrp := s.ChanGroup[node.Name]
+		f := func() {
+			logger.Infoln("func register:", node.Name)
+			msgs := make([]Message, 0)
+			for _, readCh := range chGrp.ReadCh {
+				msg := <-readCh
+				msgs = append(msgs, msg)
+			}
+
+			logger.Infoln("func launch:", node.Name)
+
+			// TODO: Check error
+			mergeMsg := &Message{}
+			for i := range msgs {
+				msgs[i].DeepMergeInto(mergeMsg)
+			}
+
+			for _, writeCh := range chGrp.WriteCh {
+				writeCh <- *mergeMsg
+			}
+
+			logger.Infoln("func end:", node.Name)
+		}
+		fs = append(fs, f)
+	}
+	return fs
+}
